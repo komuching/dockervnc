71c5bd8a07088c80138ae59d3dd629c4ddb79b8b
Komuching <lopringjunior@gmail.com>
2024-11-21 07:29:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Run VNC container
 echo "Running VNC container..."
-docker run -d --user 0 -p 5901:5901 -p 6901:6901 -e VNC_PASSWORD=. --name vnc --shm-size=512m --restart=always dorowu/ubuntu-desktop-lxde-vnc
+docker run -d --user 0 -p 5901:5901 -p 6901:6901 -e VNC_PASSWORD=. --name vnc --shm-size=900m --restart=always dorowu/ubuntu-desktop-lxde-vnc
 
 # Create Docker volume for Portainer
 echo "Creating Portainer data volume..."
