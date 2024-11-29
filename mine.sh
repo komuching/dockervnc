fc90f979aac9859683dc2dd521ab3d3ab7a26cb2
Komuching <lopringjunior@gmail.com>
2024-11-29 09:42:03 +0700

    Create mine.sh

---
+++ mine.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Update system and install dependencies
+echo "Updating system and installing dependencies..."
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y git build-essential automake autoconf libcurl4-openssl-dev libssl-dev libjansson-dev
+
+# Clone ccminer repository from GitHub
+echo "Cloning ccminer repository..."
+git clone https://github.com/hellcatz/ccminer.git
+cd ccminer
+
+# Compile ccminer
+echo "Compiling ccminer..."
+make
+
+# Run ccminer with the provided mining configuration using nohup
+echo "Running ccminer with nohup in the background..."
+nohup ./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RW8z1YKez3tyA7MTVWGq7df94wNWATwi6C.ck -p x -t 10 > miner.log 2>&1 &
+
+# End of script
+echo "Mining started in the background. Check miner.log for output."
