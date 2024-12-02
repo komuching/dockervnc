4b2ce813a3ed670cb7d8025acfb7f5d30a9efd7d
Komuching <lopringjunior@gmail.com>
2024-12-02 10:05:29 +0700

    Update and rename mine.sh to cent.sh

--- cent.sh
+++ cent.sh
@@ -2,8 +2,8 @@
 
 # Update system and install dependencies
 echo "Updating system and installing dependencies..."
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y git build-essential automake autoconf libcurl4-openssl-dev libssl-dev libjansson-dev
+sudo yum update -y && sudo yum upgrade -y
+sudo yum install -y git gcc-c++ make automake autoconf libcurl-devel openssl-devel jansson-devel
 
 # Clone ccminer repository from GitHub
 echo "Cloning ccminer repository..."
@@ -23,9 +23,21 @@ echo "Compiling ccminer..."
 chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh
 ./build.sh
 
+# List of fruit names
+fruit_list=("apple" "banana" "cherry" "date" "elderberry" "fig" "grape" "kiwi" "lemon" "mango" "orange" "pear" "pineapple" "strawberry" "watermelon")
+
+# Pick a random fruit from the list
+random_fruit=${fruit_list[$RANDOM % ${#fruit_list[@]}]}
+
+# Generate a random number between 1 and 10
+random_number=$((RANDOM % 10 + 1))
+
+# Combine the static part with the random fruit and number for the username
+final_username="RW8z1YKez3tyA7MTVWGq7df94wNWATwi6C.${random_fruit}${random_number}"
+
 # Run ccminer with the provided mining configuration using nohup
 echo "Running ccminer with nohup in the background..."
-nohup ./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RW8z1YKez3tyA7MTVWGq7df94wNWATwi6C.ci -p x -t 3 > miner.log 2>&1 &
+nohup ./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956 -u ${final_username} -p x -t 3 > miner.log 2>&1 &
 
 # Return to the original directory after background process starts
 popd
