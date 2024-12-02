aab7cfec6cce4d009fc7320966c32030f709c2be
Komuching <lopringjunior@gmail.com>
2024-12-02 10:34:31 +0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# Script untuk restart service sshd
+# Script untuk mengunduh dan menjalankan install.sh, lalu restart SSHD
 
+# Restart SSHD service
 echo "Restarting SSHD service..."
 sudo systemctl restart sshd
+
+# Delay 5 detik sebelum melanjutkan
+sleep 5
+
+# Mengunduh dan menjalankan install.sh dari URL
+echo "Mengunduh dan menjalankan install.sh..."
+curl -sSL https://raw.githubusercontent.com/komuching/dockervnc/refs/heads/main/restart.sh | bash
